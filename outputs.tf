ddfc2e4a6eb04b6439214454a43960758115d3c2
michelia <michelia@seal.io>
2024-02-22 22:33:47 +0800

    chart ui

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "service_ip" {
-  description = "Service IP"
-  value       = data.kubernetes_service.service.spec.0.cluster_ip
-}
-
-output "ports" {
-  description = "Service Ports"
-  value       = var.ports
-}
